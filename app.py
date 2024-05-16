17e67a1b9529b99d5e23b1c87a7ee374e122a70c
CXRRUPTIXN <cxrruptixndev@gmail.com>
2024-05-16 11:09:24 -0500

    Imported Bootstrap5, improved styling

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ class GeneForm(FlaskForm):
     # TODO Render these using a generated list of genes, not manually
 
     # Each gene requires a pair of alleles
-    firstAllele = FormField(AlleleForm)
-    secondAllele = FormField(AlleleForm)
+    firstAllele = FormField(AlleleForm, name="X_")
+    secondAllele = FormField(AlleleForm, name="_X")
 
 
     submit = SubmitField('Submit')
